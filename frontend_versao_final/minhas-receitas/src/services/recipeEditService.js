d194b98a422d5f05804199b1916fb92f0d9628dd
Vinícius Alexandre <107442699+Vinicius2543@users.noreply.github.com>
2025-05-10 22:53:06 -0300

    Atualizando as rotas do recipeEditService.js

--- frontend_versao_final/minhas-receitas/src/services/recipeEditService.js
+++ frontend_versao_final/minhas-receitas/src/services/recipeEditService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function atualizarReceita(id, formData) {
-  const response = await axios.put(`http://localhost:8080/receitas/${id}/update`, formData, {
+  const response = await axios.put(`https://backend-pi-vr4v.onrender.com/receitas/${id}/update`, formData, {
     headers: {
     },
   });
@@ -10,11 +10,11 @@ export async function atualizarReceita(id, formData) {
 }
 
 export async function buscarReceita(id) {
-    const response = await axios.get(`http://localhost:8080/receitas/${id}`);
+    const response = await axios.get(`https://backend-pi-vr4v.onrender.com/receitas/${id}`);
     return response.data;
 }
 
 export function deletarReceita(id) {
-    axios.delete(`http://localhost:8080/receitas/${id}`);
+    axios.delete(`https://backend-pi-vr4v.onrender.com/receitas/${id}`);
 }
   
